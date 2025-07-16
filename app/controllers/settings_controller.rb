964c502322df51fd2a1b78e75e6d3ef0148ec0a0
GlennIvander <ivander792@gmail.com>
2025-07-16 16:46:05 +0800

    modified settings form

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,5 +3,15 @@ class SettingsController < ApplicationController
   end
 
   def create
+    if current_user.update(user_params)
+      redirect_to root_path, notice: "Updated successfully"
+    else
+      render :show
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address_1, :address_2, :city, :region, :zipcode, :country)
   end
 end
